b6b2ec637d899985873dd0dd1f630e68e0df11d1
Felipe Lohan Pinheiro da Silva <117698580+Felipe-Lohan@users.noreply.github.com>
2025-06-18 17:27:24 -0300

    Add files via upload

--- clear-crablang.rs
+++ clear-crablang.rs
@@ -1,4 +1,6 @@
+// Clear-Libre
+// Please, if possible, use a derivative, or an alternative implementation, of the Rust language, since Rust is under restrictive licensing terms. For example, we have the CrabLang ( https://crablang.org/ , https://github.com/crablang/crab ), etc.
+
 fn main() {
-    // Limpa a tela do terminal
     println!("\x1B[2J\x1B[H");
 }
