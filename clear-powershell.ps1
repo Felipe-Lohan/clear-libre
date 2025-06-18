895d67750481d7d5a84b82f84a10d7a37c03acb4
Felipe Lohan Pinheiro da Silva <117698580+Felipe-Lohan@users.noreply.github.com>
2025-06-18 17:20:52 -0300

    Add files via upload

--- clear-powershell.ps1
+++ clear-powershell.ps1
@@ -2,13 +2,13 @@
 
 $esc = [char]27
 
-# Clear screen:
-
-$clear = "${esc}[2J"
-
 # Cursor return:
 
 $cursor = "${esc}[2H"
 
+# Clear screen:
+
+$clear = "${esc}[2J"
+
 # Call it:
-Write-Host "${cursor}${clear}" -NoNewline ;
+Write-Host "${cursor}" -NoNewline; Write-Host "${clear}" -NoNewline;
